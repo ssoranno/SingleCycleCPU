4b4cd78d0c8cd449dce3c03817f232a95bb29a28
ssoranno <ssoranno363@gmail.com>
2018-04-24 15:39:50 -0400

    main testbench

--- calc_tb.vhdl
+++ calc_tb.vhdl
@@ -10,22 +10,21 @@ end calc;
 
 architecture behav of calc_tb is
 --  Declaration of the component that will be instantiated.
-component calc 
+component calc
 	port (
-		I: in std_logic_vector(3 downto 0);
+		I: in std_logic_vector(7 downto 0);
 		clk: in std_logic;
-		O: out std_logic
 	);
 end component;
 
 signal i : std_logic_vector(3 downto 0);
-signal clk, o: std_logic;
+signal clk: std_logic;
 
 constant clk_period : time := 10 ns;
 
 begin
 --  Component instantiation.
-b1: branch port map (i => I, clk => clk, o=>O);
+c1: calc port map (i => I, clk => clk);
 
 -- Clock process
 clk_process : process
