04276b0b17c71638262e5a44f921a530a7df78e7
ssoranno <ssoranno363@gmail.com>
2018-04-27 15:13:47 -0400

    edited testbench

--- calc_tb.vhdl
+++ calc_tb.vhdl
@@ -87,39 +87,55 @@ begin
 	wait until rising_edge(clk);
 	i<= "11000010";
 	
-	-- load max positive 7 to register 
+	-- load max positive 7 to register 3
 	wait until rising_edge(clk);
 	i<= "10110111";
 	
-	-- 
+	-- Display register 3
 	wait until rising_edge(clk);
 	i<= "11000011";
-	-- load min negative -8
+	
+	-- load negative -8 to register 0
 	wait until rising_edge(clk);
 	i<= "10001000";
+	
+	-- Display register 0
 	wait until rising_edge(clk);
 	i<= "11000000";
+	
 	-- add negative and 0 (-3=-3+0)(t1=t2+t1)
 	wait until rising_edge(clk);
 	i<= "00011001";
+	
+	-- display register 1
 	wait until rising_edge(clk);
 	i<= "11000001";
+	
 	-- add positive and negative (4=7+(-3))(t2=t3+t1)
 	wait until rising_edge(clk);
 	i<= "00101101";
+	
+	-- Display register 2
 	wait until rising_edge(clk);
 	i<= "11000010";
+	
 	-- add positive and positive(11=7+4)(t3=t3+t2)
 	wait until rising_edge(clk);
 	i<= "00111110";
+	
+	-- Display register 3
 	wait until rising_edge(clk);
 	i<= "11000011";
+	
 	-- add two negatives(-11=-3+(-8))(t0=t1+t0)
 	wait until rising_edge(clk);
 	i<= "00000100";
+	
+	-- Display register 0
 	wait until rising_edge(clk);
 	i<= "11000000";
-	-- Displayadd results of all registers
+	
+	-- Display add results of all registers
 	wait until rising_edge(clk);
 	i<= "11000000";
 	wait until rising_edge(clk);
@@ -128,30 +144,44 @@ begin
 	i<= "11000010";
 	wait until rising_edge(clk);
 	i<= "11000011";
+	
 	-- subtract positive numbers (7=11-4) (t3=t3-t2)
 	wait until rising_edge(clk);
 	i<= "01111110";
+	
+	-- Display register 3
 	wait until rising_edge(clk);
 	i<= "11000011";
+	
 	-- subtract positive and negative number (-10=-3-7) (t1=t1-t3)
 	wait until rising_edge(clk);
 	i<= "01010111";
+	
+	-- Display register 1
 	wait until rising_edge(clk);
 	i<= "11000001";
+	
 	-- subtract 2 negative numbers (-1=-11-(-10)) (t0=t0-t1)
 	wait until rising_edge(clk);
 	i<= "01000001";
+	
+	-- Display register 0
 	wait until rising_edge(clk);
 	i<= "11000000";
-	-- Load 0 into t0
+	
+	-- Load 0 into register 0
 	wait until rising_edge(clk);
 	i<= "10000000";
+	
 	-- subtract negative number and 0 (10=0-(-10)) (t1=t0-t1)
 	wait until rising_edge(clk);
 	i<= "01010001";
+	
+	-- Display register 1
 	wait until rising_edge(clk);
 	i<= "11000001";
-	-- Displayadd results of all registers
+	
+	-- Display subtract results of all registers
 	wait until rising_edge(clk);
 	i<= "11000000";
 	wait until rising_edge(clk);
@@ -160,48 +190,66 @@ begin
 	i<= "11000010";
 	wait until rising_edge(clk);
 	i<= "11000011";
+	
 	-- beq t0 and t1 testing false should move on to the next instruction
 	wait until rising_edge(clk);
 	i<= "11000101";
+	-- Display register 0
 	wait until rising_edge(clk);
 	i<= "11000000";
+	
 	-- load 7 into t2
 	wait until rising_edge(clk);
 	i<= "10100111";
+	
 	-- beq two equal positives and skip 1 instruction (t2 t3)
 	wait until rising_edge(clk);
 	i<= "11101101";
+	-- Display register 0 (This instruction is skipped)
 	wait until rising_edge(clk);
 	i<= "11000000";
+	-- Display register 3
 	wait until rising_edge(clk);
 	i<= "11000011";
+	
 	-- beq 0 and other register continues to next instruction (t0 t1)
 	wait until rising_edge(clk);
 	i<= "11000101";
+	-- Display register 0
 	wait until rising_edge(clk);
 	i<= "11000000";
+	
 	-- load -8 into t2
 	wait until rising_edge(clk);
 	i<= "10101000";
-	-- beq positive and negative (t2, t3) continue to next instructions
+	
+	-- beq positive and negative (t2, t3) continue to next instruction
 	wait until rising_edge(clk);
 	i<= "11101110";
+	-- Display register 3
 	wait until rising_edge(clk);
 	i<= "11000011";
+	
 	-- load -8 into t3
 	wait until rising_edge(clk);
 	i<= "10111000";
+	-- Display register 3
 	wait until rising_edge(clk);
 	i<= "11000011";
+	-- Display register 2
 	wait until rising_edge(clk);
 	i<= "11000010";
+	
 	-- beq two equal negatives (t2,t3) and jump 2 instructions
 	wait until rising_edge(clk);
 	i<= "11101110";
+	-- Display register 3 (This instruction is skipped)
 	wait until rising_edge(clk);
 	i<= "11000011";
+	-- Display register 3 (This instruction is skipped)
 	wait until rising_edge(clk);
 	i<= "11000011";
+	-- Display register 1 --> value in the register is 10
 	wait until rising_edge(clk);
 	i<= "11000001";
 	wait;
